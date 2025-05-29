21e90b304d083a840e80773f7ccf0a4c401b83e0
codinngar <codinngar@gmail.com>
2025-05-29 12:25:07 +0300

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import notesRoutes from "./routes/NotesRouter.js";
 import { connectDB } from "./config/db.js";
 
@@ -8,12 +9,19 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-connectDB();
+app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
